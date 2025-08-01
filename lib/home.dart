7a119b34143507884b7114684afedc541312cc6b
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-08-02 01:13:14 +0600

    second commit

--- lib/home.dart
+++ lib/home.dart
@@ -8,21 +8,39 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Greeting App"),
-    ),
-    body: Column(
-      children: [
-        Text("Hello, World!", style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
+        backgroundColor: Colors.green[50],
+      ),
+      backgroundColor: Colors.green[50],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Hello, World!",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrangeAccent,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text("Welcome to Flutter!", style: TextStyle(fontSize: 22)),
+            SizedBox(height: 40),
+            Image.asset("assets/images/flutter-seeklogo-01.png", width: 180),
+            SizedBox(height: 40),
+            OutlinedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text("Button Pressed")));
+              },
+              child: Text("Press Me", style: TextStyle(color: Colors.black)),
+              style: OutlinedButton.styleFrom(backgroundColor: Colors.green),
+            ),
+          ],
         ),
-        ),
-        Text("Welcome to Flutter!", style: TextStyle(
-          fontSize: 22,
-        ),
-        ),
-        Image.asset("assets/images/flutter-seeklogo.png"),
-      ],
-    ),
+      ),
     );
   }
 }
